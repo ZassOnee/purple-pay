9aa0e59faadc56cfd709b8f6b84dd3887478aa7c
ZassOnee <m77702096@gmail.com>
2025-08-04 13:58:19 +0800

    Update config.ts

--- data/config.ts
+++ data/config.ts
@@ -1,16 +1,7 @@
-export const appConfig = {
-  whatsappGroupLink: "", // link group
-  nameHost: "KuroShop", // nama host 
-  feeMin: 50, //minimal fee
-  feeMax: 300, // max fee 
-  apikey: "Kuroshop", // ga usah di ubah nanti error apikey uda premium 
-  emailSender: {
-    host: "okfajri45@gmail.com", // Gmail host
-    port: 587, // ga usa di ubah, ga guna 
-    secure: false, // false in
-    auth: {
-      user: "", // Gmail buat ngirim ke Gmail buyer 
-      pass: "", // sandi aplikasi 
-    },
-    from: "KuroShop <okfajri45@gmail.com>",
-  }, // ganti sendiri 
+export const ConfigSB = {
+  supabase: {
+    url: 'hhh',
+    anonkey: 'hhh',
+    siteid: '1',
+  }
+}
