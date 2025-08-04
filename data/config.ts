8ebc3921247a89d53b7f71ad921122002d918d8e
ZassOnee <m77702096@gmail.com>
2025-08-04 14:13:28 +0800

    Update config.ts

--- data/config.ts
+++ data/config.ts
@@ -1,7 +1,7 @@
 export const ConfigSB = {
   supabase: {
-    url: 'hhh',
-    anonkey: 'hhh',
+    url: 'https://rdekmabwmcjkvrlzdrdu.supabase.co',
+    anonkey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZWttYWJ3bWNqa3ZybHpkcmR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3MDUwMjQsImV4cCI6MjA2NjI4MTAyNH0.WC68cpA8R-Etq5936JqCLoaMa5QlWL2CVZUrDG6TNJE',
     siteid: '1',
   }
 }
